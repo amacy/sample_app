81407a3333fbe605247ff194db507538554d3908
Aaron Macy <aaronmacy@gmail.com>
2012-11-28 21:31:07 -0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,12 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
 
-	validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
@@ -24,9 +25,13 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
   
-  	def create_remember_token
-  		self.remember_token = SecureRandom.urlsafe_base64
-  	end
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
